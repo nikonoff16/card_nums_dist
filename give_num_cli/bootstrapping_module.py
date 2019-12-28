6f1089c25b4b4d80c7688fe5ac5f269f60b46460
Egor Egorov <egigoka@gmail.com>
2019-12-28 21:21:07 +0500

    bugfix, update setup.py

---
+++ give_num_cli/bootstrapping_module.py
@@ -0,0 +1,48 @@
+#! python3
+# -*- coding: utf-8 -*-
+# # # bootstrap # # # 1.5.0
+
+MODULE_TO_IMPORT = "https://github.com/egigoka/telegrame"
+
+import sys
+import os
+def download_file(url, out=None):
+    try:  # getting wget
+        import wget
+    except ModuleNotFoundError:
+        os.system("pip install wget")
+        import wget
+    import ssl
+    ssl._create_default_https_context = ssl._create_unverified_context
+    output_file_name = wget.download(url, out=out)
+    return output_file_name
+
+try:  # getting telegrame
+    import telegrame
+except (ModuleNotFoundError, ImportError):
+    # getting git
+    if os.system("git --version"):  # if get error while checking git version
+        if sys.platform == "win32":  # getting git
+            print("Downloading git, please, wait!")
+            git_file_name = download_file("http://github.com/git-for-windows/git/releases/download/v2.17.1.windows.2/Git-2.17.1.2-32-bit.exe")
+            print("Installing git, please, wait!")
+            os.system(" ".join([git_file_name, '/VERYSILENT', '/NORESTART', '/NOCANCEL', '/SP-', '/CLOSEAPPLICATIONS',
+                                '/RESTARTAPPLICATIONS', r'/COMPONENTS="icons,ext\reg\shellhere,assoc,assoc_sh"']))
+            os.environ["PATH"] = os.environ["PATH"] + r";C:\Program Files (x86)\Git\cmd;C:\Program Files\Git\cmd"
+            os.system("del " + git_file_name)
+        elif sys.platform in ["linux", "linux2"]:
+            os.system("sudo apt-get install git")  # todo test
+        elif sys.platform == "darwin":
+            os.system("brew install git")  # todo test
+        else:
+            raise NotImplementedError("OS " + sys.platform + " is not supported")
+    # end getting git
+    comms = f"python3 -m pip install git+{MODULE_TO_IMPORT}"
+    print('......')
+    print(comms)
+    print('......')
+
+    os.system(f"pip install git+{MODULE_TO_IMPORT}")
+
+    import telegrame
+# # # end bootstrap # # #
---
+++ give_num_cli/telebot.py
@@ -0,0 +1,1 @@
+from bootstrapping_module import *
