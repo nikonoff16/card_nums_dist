c0f1d4d836c293ffc1bea715143c7130442bb7e5
Egor Egorov <egigoka@gmail.com>
2019-12-29 13:08:26 +0500

    small upd

--- give_num_cli/archived_nums.py
+++ give_num_cli/archived_nums.py
@@ -2,7 +2,7 @@ from commands import *
 from givenumber import script_dir
 
 
-def check_archive(integer, verbose=True):
+def check_archive(integer, verbose=False):
     if verbose:
         b = Bench(quiet=False, fraction_digits=3)
 
@@ -10,7 +10,7 @@ def check_archive(integer, verbose=True):
     sub_archive = string[:-4]+"0k"
     if not sub_archive:
         sub_archive = "0"
-    archive_file = Path.combine(script_dir, 'archive', f"starting_'{sub_archive}'.txt")
+    archive_file = Path.combine(script_dir, 'archive', f"starting_'{sub_archive}'.123")
 
     if not File.exist(archive_file):
         File.create(archive_file)
@@ -32,13 +32,5 @@ def check_archive(integer, verbose=True):
 
 
 if __name__ == '__main__':
-    #bm = Bench("create 1m entries", quiet=False)
-    #bk = Bench("create 10k entries ", quiet=False)
-    #for i in Int.from_to(1, 1*1000*1000):
-    #    if i%10000 == 0:
-    #        bk.prefix = bk.prefix[:19] + str(i)
-    #        bk.end()
-    #    check_archive(i, verbose=False)
-    #bm.end()
     while True:
-        check_archive(Str.input_int())
+        check_archive(Str.input_int(), verbose=True)
